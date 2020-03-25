0c3a07b50311e2f878e6df6dbc18dc29fc461fa9
Jan-Jasper van der Linde (DutchWorkz BV) <Jan-Jasper.van.der.linde@dutchworkz.nl>
2020-03-25 19:33:12 +0100

    fix git

---
+++ TANP.Lib/Model/Basket.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+
+namespace TANP.Lib.Model
+{
+   public  class Basket
+    {
+        public int CustomerId { get; set; }
+        public List<Product> ProductItems { get; set; } = new List<Product>();
+        public decimal TotalAmount => ProductItems?.Sum(p => p.Price)??0;
+    }
+}
---
+++ TANP.Lib/Model/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TANP.Lib.Model
+{
+    public class Product
+    {
+        public int ProductNumber { get; set;  }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+    }
+}
